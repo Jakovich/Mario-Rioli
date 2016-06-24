f0605710e58ae31a705e4a8507841c4ce94c560d
Nadia Pillard <ua9ond@yandex.ru>
2016-06-24 23:19:49 +0600

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require("gulp");
+
+var server = require("browser-sync");
+
+gulp.task("show", function(){
+  server.init({
+    server: ".",
+    notify: false,
+    open: true,
+    ui: false
+  });
+  
+  
+  gulp.watch("*.html").on("change", server.reload);
+  gulp.watch("js/*.js").on("change", server.reload);
+  gulp.watch("css/*.css").on("change", server.reload);
+  
+});
+
+
