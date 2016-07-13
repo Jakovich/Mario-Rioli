73c422b49394af11028ed715a4a7dfea17e0e1f4
Nadia Pillard <ua9ond@yandex.ru>
2016-07-13 20:13:14 +0600

    build tasks added

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,47 @@ var gulp = require("gulp");
 
 var server = require("browser-sync");
 
+var uglify = require("gulp-uglify");
+
+var rename = require("gulp-rename");
+
+var copy = require("gulp-contrib-copy");
+
+var clean = require("gulp-contrib-clean");
+
+var csso = require("gulp-csso");
+
+gulp.task("minjs", function() {
+  gulp.src("./js/main.js")
+    .pipe(gulp.dest("build/js/"))
+    .pipe(rename("main.min.js"))
+    .pipe(uglify())
+    .pipe(gulp.dest("build/js/"))
+});
+
+gulp.task("style", function(){
+  gulp.src("css/style.css")
+  .pipe(gulp.dest("build/css"))
+  .pipe(rename("style.min.css"))
+  .pipe(csso())
+  .pipe(gulp.dest("build/css"))
+  .pipe(server.reload({stream: true}));
+});
+
+gulp.task("copyHtml", function() {
+  gulp.src("*.html")
+  .pipe(copy())
+  .pipe(gulp.dest("build"))
+  gulp.src("./img/**")
+  .pipe(copy())
+  .pipe(gulp.dest("build/img"))
+});
+
+gulp.task("clean", function () {
+  return gulp.src("build", {read: false})
+    .pipe(clean());
+});
+
 gulp.task("show", function(){
   server.init({
     server: ".",
@@ -16,5 +57,7 @@ gulp.task("show", function(){
   gulp.watch("css/*.css").on("change", server.reload);
   
 });
+
+gulp.task("build", ["clean", "copyHtml", "minjs", "style"]);
 
 
