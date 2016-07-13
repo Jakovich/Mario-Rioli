5a758ee075dc6b066644c9a63cb9a1bdff789eff
Nadia Pillard <ua9ond@yandex.ru>
2016-07-13 20:37:51 +0600

    load gif changed

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 $(document).ready(function() {
   
+  
+  
   /*функция смены картинок двери*/
   $('.doors-item__color-btn').click(function(){
     $currentButton = $(this);
     var $currentSrc = $currentButton.attr('data-src');
     var $currentImg = $currentButton.parents('.doors-item').find('img');
     var imageReplace = new Image();
+    
     imageReplace.onload = function() {
       $currentImg.attr('src', $currentSrc);
     }
